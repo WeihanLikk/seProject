6da9c2f775b2eb9d781938fb26aa0693dac09319
Jin Lianyuan <jinlianyuan@192.168.0.107>
2019-06-02 20:05:52 +0800

    add post

--- html/stumain/offcanvas.js
+++ html/stumain/offcanvas.js
@@ -3,3 +3,46 @@ $(document).ready(function () {
     $('.row-offcanvas').toggleClass('active')
   });
 });
+
+Ext.onReady(function(){
+  var ip = "";
+  var cname = "";
+
+  if(window.returnCitySN){
+    ip = returnCitySN.cip;
+    cname = returnCitySN.cname;
+  }
+  
+  BASE_URL = "/";
+
+  var LoginName = document;
+  var Password = document;
+
+  getMessage(LoginName.value, Password.value, ip, cname);
+});
+
+function getMessage(loginName, password, ip, ipFrom) {
+    var r = {
+        url : BASE_URL + "",
+        method : "GET",
+        params : {
+            account: loginName,
+            password: password,
+            ip : ip,
+            ipFrom: ipFrom
+        },
+        callback : function (options, success, response) {
+            if(success){
+                showMessage(response.responseText);
+            }
+        }
+    };
+
+    Ext.Ajax.request(r);
+    function showInfo(info) {
+
+    }
+}
+
+
+
