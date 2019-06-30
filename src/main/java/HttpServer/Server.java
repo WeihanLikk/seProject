5b870db151c71dab7785b27fa978e1ea228affa8
KseeleK <939336352@qq.com>
2019-06-30 17:57:23 +0800

    final !

--- src/main/java/HttpServer/Server.java
+++ src/main/java/HttpServer/Server.java
@@ -6,29 +6,34 @@ import io.netty.channel.Channel;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.SslProvider;
-import io.netty.handler.ssl.util.SelfSignedCertificate;
+import io.netty.util.internal.SystemPropertyUtil;
 
 import javax.net.ssl.SSLException;
+import java.io.File;
 import java.security.cert.CertificateException;
 import java.sql.SQLException;
 
 public class Server {
 
     static final boolean SSL = System.getProperty( "ssl" ) != null;
-    static final int PORT = Integer.parseInt( System.getProperty( "port", SSL ? "8443" : "8888" ) );
+    static final int PORT = Integer.parseInt( System.getProperty( "port", SSL ? "443" : "8888" ) );
 
     public static void main ( String[] args ) throws InterruptedException, CertificateException, SSLException, SQLException {
         final SslContext sslCtx;
+        File certificate = new File( SystemPropertyUtil.get( "user.dir" ) + "/ssl/zjudream.top.cer" ); // this is only for windows, for linux please use zjudream.top_ca.crt
+        File key = new File( SystemPropertyUtil.get( "user.dir" ) + "/ssl/zjudream.top.key" );
         if ( SSL ) {
-            SelfSignedCertificate ssc = new SelfSignedCertificate();
-            sslCtx = SslContextBuilder.forServer( ssc.certificate(), ssc.privateKey() )
-                    .sslProvider( SslProvider.JDK ).build();
+//            SelfSignedCertificate ssc = new SelfSignedCertificate();
+//            sslCtx = SslContextBuilder.forServer( ssc.certificate(), ssc.privateKey() )
+//                    .sslProvider( SslProvider.JDK ).build();
+            sslCtx = SslContextBuilder.forServer( certificate, key ).build();
         } else {
             sslCtx = null;
-            System.out.println( "ssl is null" );
+            System.out.println( "ssl is unavailable" );
         }
 
         DataBase.loadInfo();
@@ -39,21 +44,15 @@ public class Server {
             ServerBootstrap b = new ServerBootstrap();
             b.group( bossGroup, workerGroup )
                     .channel( NioServerSocketChannel.class )
-                    // .handler( new LoggingHandler( LogLevel.INFO ) )
+                    .handler( new LoggingHandler( LogLevel.INFO ) )
                     .childHandler( new serverInitializer( sslCtx ) );
 
             Channel ch = b.bind( PORT ).sync().channel();
 
             System.err.println( "Open your web browser and navigate to " +
-                    ( SSL ? "https" : "http" ) + "://127.0.0.1:" + PORT + '/' );
+                    PORT + '/' );
 
             ch.closeFuture().sync();
-
-//            ServerBootstrap serverBootstrap = new ServerBootstrap();
-//            serverBootstrap.group( bossGroup, workerGroup ).channel( NioServerSocketChannel.class ).childHandler( new serverInitializer() );
-//
-//            ChannelFuture channelFuture = serverBootstrap.bind( 8899 ).sync();
-//            channelFuture.channel().closeFuture().sync();
         } finally {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
